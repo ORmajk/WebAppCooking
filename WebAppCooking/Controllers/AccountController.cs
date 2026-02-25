5109c8ee9d2758f80eeaa8e603ecafcc47abe8c7
mandz <mandzievarslan5@gmail.com>
2026-02-25 12:49:20 +0300

    25

---
+++ WebAppCooking/Controllers/AccountController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAppCooking.Data;
+using WebAppCooking.Models;
+
+public class AccountController : Controller
+{
+    private readonly UserAppContext _context;
+
+    public AccountController(UserAppContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Login(LoginViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var user = _context.Users
+                .Include(u => u.Role)
+                .FirstOrDefault(u => u.Login == model.Login && u.Password == model.Password);
+
+            if (user != null)
+            {
+                // Простая аутентификация через сессию
+                HttpContext.Session.SetString("UserId", user.IdUser.ToString());
+                HttpContext.Session.SetString("UserLogin", user.Login);
+                HttpContext.Session.SetString("UserName", user.Name);
+                HttpContext.Session.SetString("UserRole", user.Role?.RoleName ?? "");
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError("", "Неверный логин или пароль");
+        }
+        return View(model);
+    }
+
+    [HttpGet]
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Register(RegisterViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            // Проверяем, существует ли уже пользователь с таким логином
+            var existingUser = _context.Users.FirstOrDefault(u => u.Login == model.Login);
+            if (existingUser != null)
+            {
+                ModelState.AddModelError("Login", "Пользователь с таким логином уже существует");
+                return View(model);
+            }
+
+            // Создаем нового пользователя
+            var user = new User
+            {
+                Login = model.Login,
+                Password = model.Password, // В реальном проекте нужно хешировать пароль!
+                Name = model.Name,
+                Age = model.Age,
+                IdRole = 1 // Обычная роль пользователя (предполагаем, что IdRole=1 - это обычный пользователь)
+            };
+
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
+            // Автоматически логиним после регистрации
+            HttpContext.Session.SetString("UserId", user.IdUser.ToString());
+            HttpContext.Session.SetString("UserLogin", user.Login);
+            HttpContext.Session.SetString("UserName", user.Name);
+
+            return RedirectToAction("Index", "Home");
+        }
+        return View(model);
+    }
+
+    public IActionResult Logout()
+    {
+        HttpContext.Session.Clear();
+        return RedirectToAction("Login");
+    }
+}
---
+++ WebAppCooking/Models/RegisterViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppCooking.Models
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Введите логин")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Логин должен быть от 3 до 50 символов")]
+        [Display(Name = "Логин")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Введите пароль")]
+        [DataType(DataType.Password)]
+        [StringLength(50, MinimumLength = 4, ErrorMessage = "Пароль должен быть от 4 до 50 символов")]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Подтвердите пароль")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        [Display(Name = "Подтверждение пароля")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Введите имя")]
+        [StringLength(50)]
+        [Display(Name = "Имя")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Введите возраст")]
+        [Range(1, 120, ErrorMessage = "Возраст должен быть от 1 до 120")]
+        [Display(Name = "Возраст")]
+        public int Age { get; set; }
+    }
+}
